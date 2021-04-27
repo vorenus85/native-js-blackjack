999d41f6a16f89d2ebd0588f9c029eb76ff01ff2
Janos Perge <janos.perge@innonic.com>
2021-04-27 22:00:21 +0200

    code reformat

--- src/js/blackJack.js
+++ src/js/blackJack.js
@@ -1,48 +1,61 @@
-
 /**
  * Simple JS module to export
  */
- export const blackJack = (function () {
-   const deck = initDeck();
-   function initDeck(){
-     const deck = [];
-     const colors = ['H','D','C','S'];
-     const cards = ['2','3','4','5','6','7','8','9','10','A','K','Q','J'];
-     for (let colorIndex = 0; colorIndex < colors.length; colorIndex++) {
-       const colorElement = colors[colorIndex];
-       for (let cardIndex = 0; cardIndex < cards.length; cardIndex++) {
+export const blackJack = (function () {
+  const deck = initDeck();
+  function initDeck() {
+    const deck = [];
+    const colors = ["H", "D", "C", "S"];
+    const cards = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "A",
+      "K",
+      "Q",
+      "J",
+    ];
+    for (let colorIndex = 0; colorIndex < colors.length; colorIndex++) {
+      const colorElement = colors[colorIndex];
+      for (let cardIndex = 0; cardIndex < cards.length; cardIndex++) {
         const cardElement = cards[cardIndex];
-        deck.push(cardElement+colorElement)
+        deck.push(cardElement + colorElement);
       }
-     }
-     return deck;
-   }
-   function shuffleDeck(){
-      const array = deck;
-      //The Fisher-Yates algorith
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
-   }
-   function render(){
-    const app = document.getElementById('app');
-    const deck = shuffleDeck()
+    }
+    return deck;
+  }
+  function shuffleDeck() {
+    const array = deck;
+    //The Fisher-Yates algorith
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+  function render() {
+    const app = document.getElementById("app");
+    const deck = shuffleDeck();
     for (let index = 0; index < deck.length; index++) {
       const cardID = deck[index];
-      let cardElement = document.createElement('div');
+      let cardElement = document.createElement("div");
       cardElement.classList.add("h-16", "m-2", "pokercard", "col");
-      cardElement.setAttribute('id', 'card-'+cardID);
+      cardElement.setAttribute("id", "card-" + cardID);
       app.appendChild(cardElement);
     }
-   }
-   return {
-     deck: deck,
-     render: render,
-     shuffle: shuffleDeck,
-     init: initDeck
-   }
-  }());
+  }
+  return {
+    deck: deck,
+    render: render,
+    shuffle: shuffleDeck,
+    init: initDeck,
+  };
+})();
